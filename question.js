8e29a840da1747cc4de867de84c5f5f33641b052
Benjamin Fiadonu <benjaminfiadonu@Benjamins-Air.home>
2022-05-26 22:18:44 -0400

    first commit

---
+++ question.js
@@ -0,0 +1,29 @@
+var questions = [{
+    "question": "What is my name?",
+    "option1": "Benjamin",
+    "option2": "Kofi",
+    "option3": "Franklin",
+    "option4": "Peter",
+    "answer": "1"
+}, {
+    "question": "What is my age?",
+    "option1": "12",
+    "option2": "13",
+    "option3": "15",
+    "option4": "26",
+    "answer": "4"
+}, {
+    "question": "What is my sister's name?",
+    "option1": "Ama",
+    "option2": "Sarah",
+    "option3": "Whitney",
+    "option4": "Michelle",
+    "answer": "4"
+}, {
+    "question": "What is my Mother's Name?",
+    "option1": "Philipa",
+    "option2": "Margaret",
+    "option3": "Roseline",
+    "option4": "Claudia",
+    "answer": "2"
+}]
